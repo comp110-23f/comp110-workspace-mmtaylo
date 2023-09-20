c444b9bbffb188fa3c335d705e5f21294dd79f80
Megan Taylor <mlltaylor.m@gmail.com>
2023-09-20 15:18:54 -0400

    made corrections to ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -19,7 +19,7 @@ guessed_word: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
 # while loop for checking if the length of the guessed word is the same as the length of the secret word - if not, try again
 while len(guessed_word) > len(secret_word) or len(guessed_word) < len(secret_word):
-    guessed_word: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+    guessed_word = str(input(f"That was not {len(secret_word)} letters! Try again: "))
 
 # checking each index in both the secret and guessed words, printing and storing a green box in guess_track if they match
 while check_idx < len(secret_word):
@@ -27,13 +27,12 @@ while check_idx < len(secret_word):
         guess_track = guess_track + green_box
     else:
         # while a character is in the guessed word that is also in the secret word and the alternate check is less than the length of the secret word... checking if the indexes match or not, then indicing (either resulting in a yellow or white box for the current index)
-        while character_existence != True and altcheck_idx < len(secret_word):
+        while character_existence is not True and altcheck_idx < len(secret_word):
             if guessed_word[check_idx] == secret_word[altcheck_idx]:
                 character_existence = True
             else:
                 altcheck_idx = altcheck_idx + 1
-
-        if character_existence == True:
+        if character_existence is True:
             guess_track = guess_track + yellow_box
         else:
             guess_track = guess_track + white_box
