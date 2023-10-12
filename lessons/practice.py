6d5f63182d19e64b3f2674eb0812e98e876483f4
Megan Taylor <mlltaylor.m@gmail.com>
2023-10-12 15:30:37 -0400

    lessons practice

---
+++ lessons/practice.py
@@ -0,0 +1,6 @@
+"""Demonstrates me trying my best."""
+x: str = "else"
+if len(x) == 1:
+    print("boooo")
+else:
+    print(x)
---
+++ lessons/u_da_best.py
@@ -0,0 +1,5 @@
+"""Demonstrates being super nice"""
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", happy game day!")
+print("You are the best Mellophone ever, " + user_name + "! :3")
